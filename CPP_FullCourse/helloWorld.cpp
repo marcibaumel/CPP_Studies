680cd5f9d743487dc515876da1d4ecd3f639ac2c
Marci <marcibaumel@gmail.com>
2024-06-05 16:12:58 +0200

    press enter to end xd

--- CPP_FullCourse/helloWorld.cpp
+++ CPP_FullCourse/helloWorld.cpp
@@ -2,13 +2,23 @@
 
 int main()
 {
-    //Comment
-
-    /*
-    long comment
-    */
-
+    // Basic print line
     std::cout << "Hello World" << std::endl;
     std::cout << "Meh" << '\n';
+
+    // Variables
+    int x; // declaration
+    x = 5; // assignment
+
+    int y = 4;
+
+    std::cout << x + y << std::endl;
+
+    do
+    {
+        std::cout << '\n'
+                  << "Press a key to continue...";
+    } while (std::cin.get() != '\n');
+
     return 0;
 }
