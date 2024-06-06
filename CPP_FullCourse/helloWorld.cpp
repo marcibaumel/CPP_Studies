af9ad9fb17023725147785496c88fd3aedbbde5c
Marci <marcibaumel@gmail.com>
2024-06-06 16:00:37 +0200

    variables

--- CPP_FullCourse/helloWorld.cpp
+++ CPP_FullCourse/helloWorld.cpp
@@ -12,7 +12,36 @@ int main()
 
     int y = 4;
 
+    std::cout << x << std::endl;
+    std::cout << y << std::endl;
+
     std::cout << x + y << std::endl;
+
+    float days = 7.5;
+    std::cout << days << std::endl;
+
+    double gpa = 2.42542525;
+    std::cout << gpa << std::endl;
+
+    char grade = 'A';
+    char initial = 'B';
+
+    std::cout << grade << std::endl;
+    std::cout << initial << std::endl;
+
+    bool studentStatus = false;
+    bool power = true;
+
+    std::cout << studentStatus << std::endl;
+    std::cout << power << std::endl;
+    std::cout << (studentStatus && power) << std::endl;
+
+    std::string name = "Marci";
+    std::string food = "Pizza";
+
+    std::cout << food << std::endl;
+    std::cout << "Hello " << name << std::endl;
+    std::cout << "GPA: " << gpa << std::endl;
 
     do
     {
